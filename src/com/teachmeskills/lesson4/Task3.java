129aca5146ca5c9e2dc7ea167885abf5539bf84f
KatyaKolot <95370491+KatyaKolot@users.noreply.github.com>
2021-12-14 22:45:09 +0400

    Add files via upload

--- src/com/teachmeskills/lesson4/Task3.java
+++ src/com/teachmeskills/lesson4/Task3.java
@@ -3,12 +3,14 @@ package com.teachmeskills.lesson4;
 public class Task3 {
     public static void main(String[] args) {
 
-    int [][] myArray = {{3,1,7,5,}, {7, 9, 3,5 }};
-    int sum = 0;
-      for  (int i=0; i < myArray.length;i++ ) {
-          for (int j=0; j < myArray[i].length; j++)
-              sum = myArray [i][j]+sum; }
-          { System.out.println(sum);
-          }
-      }
+        int[][] myArray = {{3, 1, 7, 5,}, {7, 9, 3, 5}};
+        int sum = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++)
+                sum = myArray[i][j] + sum;
+        }
+
+            System.out.println(sum);
+
+    }
 }
